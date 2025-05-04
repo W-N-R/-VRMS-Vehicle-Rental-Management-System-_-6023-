b14e0770f920620e44cb26b03c0de98b41cc5d95
W-N-R <waleednaeem133@gmail.com>
2025-05-04 17:54:03 +0500

    .

--- Date.cpp
+++ Date.cpp
@@ -2,4 +2,90 @@
 
 Date::Date(int d, int m, int y)
 {
+	day = d;
+	month = m;
+	year = y;
 }
+
+int Date::getDay() const
+{
+	return day;
+}
+
+int Date::getMonth() const
+{
+	return month;
+}
+
+int Date::getYear() const
+{
+	return year;
+}
+
+void Date::setDay(int d)
+{
+	day = d;
+}
+
+void Date::setMonth(int m)
+{
+	month = m;
+}
+
+void Date::setYear(int y)
+{
+	year = y;
+}
+
+int Date::operator-(const Date& other) const
+{
+	time_t thisTime = this->toTimeT();
+	time_t otherTime = other.toTimeT();
+	return difftime(thisTime, otherTime) / (60 * 60 * 24);
+}
+
+bool Date::operator<(const Date& other) const
+{
+	if (year != other.year) {
+		return year < other.year;
+	}
+	if (month != other.month) {
+		return month < other.month;
+	}
+	return day < other.day;
+}
+
+bool Date::operator>(const Date& other) const {
+	 return other < *this; 
+}
+
+bool Date::operator<=(const Date& other) const
+{
+	 return !(*this > other); 
+}
+
+bool Date::operator>=(const Date& other) const
+{
+	return !(*this < other);
+}
+bool Date::operator==(const Date& other) const
+{
+	return day == other.day && month == other.month && year == other.year;
+}
+bool Date::operator!=(const Date& other) const
+{
+	return !(*this == other);
+}
+istream& operator>>(istream& is, Date& date)
+{
+	char separator;
+	is >> date.day >> separator >> date.month >> separator >> date.year;
+	return is;
+}
+
+ostream& operator<<(ostream& os, const Date& date)
+{
+	os << date.day << "/" << date.month << "/" << date.year;
+	return os;
+}
+
--- Date.h
+++ Date.h
@@ -16,18 +16,13 @@ private:
 
 public:
     Date(int d = 1, int m = 1, int y = 2000);
+    int getDay() const;
+    int getMonth() const;
+    int getYear() const;
+    void setDay(int d);
+    void setMonth(int m);
+    void setYear(int y) ;
 
-    // Getter methods
-    int getDay() const { return day; }
-    int getMonth() const { return month; }
-    int getYear() const { return year; }
-
-    // Setter methods
-    void setDay(int d) { day = d; }
-    void setMonth(int m) { month = m; }
-    void setYear(int y) { year = y; }
-
-    // Convert Date to time_t
     time_t toTimeT() const {
         struct tm tm = { 0 };
         tm.tm_year = year - 1900;
@@ -36,40 +31,15 @@ public:
         return mktime(&tm);
     }
 
-    // Operator overloading for date difference (in days)
-    int operator-(const Date& other) const {
-        time_t thisTime = this->toTimeT();
-        time_t otherTime = other.toTimeT();
-        return difftime(thisTime, otherTime) / (60 * 60 * 24); // Convert seconds to days
-    }
-
-    // Input operator overloading
-    friend istream& operator>>(istream& is, Date& date) {
-        char separator;
-        is >> date.day >> separator >> date.month >> separator >> date.year;
-        return is;
-    }
-
-    // Output operator overloading
-    friend ostream& operator<<(ostream& os, const Date& date) {
-        os << date.day << "/" << date.month << "/" << date.year;
-        return os;
-    }
-
-    // Comparison operators
-    bool operator<(const Date& other) const {
-        if (year != other.year) return year < other.year;
-        if (month != other.month) return month < other.month;
-        return day < other.day;
-    }
-
-    bool operator>(const Date& other) const { return other < *this; }
-    bool operator<=(const Date& other) const { return !(*this > other); }
-    bool operator>=(const Date& other) const { return !(*this < other); }
-    bool operator==(const Date& other) const {
-        return day == other.day && month == other.month && year == other.year;
-    }
-    bool operator!=(const Date& other) const { return !(*this == other); }
+    int operator-(const Date& other) const;
+    friend istream& operator>>(istream& is, Date& date);
+    friend ostream& operator<<(ostream& os, const Date& date);
+    bool operator<(const Date& other) const;
+    bool operator>(const Date& other) const;
+    bool operator<=(const Date& other) const;
+    bool operator>=(const Date& other) const;
+    bool operator==(const Date& other) const;
+    bool operator!=(const Date& other) const;
 };
 
 #endif // DATE_H
